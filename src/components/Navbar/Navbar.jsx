d33c5a74ef17e33b1c34d85ee7314de180b4e014
Meistaaa <alimuhammad9868@gmail.com>
2023-02-22 05:45:20 +0500

    gerich navbar almost

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React from "react";
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
-import './Navbar.css';
-
+import "./Navbar.css";
+import images from "../../constants/images";
 const Navbar = () => (
-  <div>
-    Navbar
+  <div className="app__navbar">
+    <div className="app__navbar-logo">
+      <img
+        className="app__navbar-image"
+        src={images.gericht}
+        alt="gericht-image"
+      />
+    </div>
+    <div className="app__links ">
+      <a className="p__opensans" href="#">
+        Home
+      </a>
+      <a className="p__opensans" href="#">
+        About
+      </a>
+      <a className="p__opensans" href="">
+        Menu
+      </a>
+      <a className="p__opensans" href="">
+        Awards
+      </a>
+      <a className="p__opensans" href="">
+        Contact
+      </a>
+    </div>
+    <div className="app__navbar-login">
+      <a href="#" className="p__opensans app__nav-login-link" type="button">
+        Login / Register
+      </a>
+      <a href="#" className="p__opensans " type="button">
+        Book Table
+      </a>
+    </div>
   </div>
 );
 
