fd71b1c076e76cacceaaeb147306289a6dedf437
Meistaaa <alimuhammad9868@gmail.com>
2023-02-23 03:11:38 +0500

    header done almost

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,37 @@
 import React from "react";
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
 import "./Navbar.css";
 import images from "../../constants/images";
 const Navbar = () => (
   <div className="app__navbar">
     <div className="app__navbar-logo">
-      <img
-        className="app__navbar-image"
-        src={images.gericht}
-        alt="gericht-image"
-      />
+      <img className="app__navbar-image" src={images.gericht} alt="gericht" />
     </div>
     <div className="app__links ">
-      <a className="p__opensans" href="#">
+      <a className="p__opensans" href="#home">
         Home
       </a>
-      <a className="p__opensans" href="#">
+      <a className="p__opensans" href="#About">
         About
       </a>
-      <a className="p__opensans" href="">
+      <a className="p__opensans" href="Menu">
         Menu
       </a>
-      <a className="p__opensans" href="">
+      <a className="p__opensans" href="Awards">
         Awards
       </a>
-      <a className="p__opensans" href="">
+      <a className="p__opensans" href="Contact">
         Contact
       </a>
     </div>
     <div className="app__navbar-login">
-      <a href="#" className="p__opensans " type="button">
+      <a href="#Login" className="p__opensans " type="button">
         Login / Register
       </a>
       <p className="vertical-line"></p>
-      <a href="#" className="p__opensans " type="button">
+      <a href="#Booking" className="p__opensans " type="button">
         Book Table
       </a>
     </div>
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
-
+import "./Header.css";
+import images from "../../constants/images";
+import { SubHeading } from "../../components";
 const Header = () => (
-  <div>
-    Header
+  <div className="section__padding app__header">
+    <div className="app__header-container">
+      <div className="app__header-content">
+        <SubHeading title="Chase the new flavour" />
+
+        <h1 className="  app__header-heading">The Key To Fine Dining</h1>
+        <p className="p__opensans  ">
+          Sit Tellus Lobortis Sed Senectus Vivamus Molestie. Condimentum
+          Volutpat Morbi Facilisis Quam Scelerisque Sapien. Et, Penatibus
+          Aliquam Amet Tellus
+        </p>
+        <button className="custom__button">Explore Menu</button>
+      </div>
+      <div>
+        <img className="app__header-img" src={images.welcome} alt="" />
+      </div>
+    </div>
   </div>
 );
 
