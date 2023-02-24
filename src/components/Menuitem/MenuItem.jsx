7de7e30e4c566ca7327e9764dc6539a6cd0f34c4
Meistaaa <alimuhammad9868@gmail.com>
2023-02-25 04:14:09 +0500

    menu section

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import './MenuItem.css';
+import "./MenuItem.css";
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({ title, price, tags }) => (
+  <div className="app__menuitems ">
+    <div className="app__menuitems-content">
+      <div className="app__menuitems-tag">
+        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+          {title}
+        </p>
+        <p className="p__opensans " style={{ color: "#AAAAAA" }}>
+          { tags }
+        </p>
+      </div>
+
+      <div className="app__menuitems-price">
+        <p className="app__menuitems-dash"></p>
+        <p className=" p__opensans">{price}</p>
+      </div>
+    </div>
   </div>
 );
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
+import { SubHeading } from "../../components";
+import MenuItem from "../../components/Menuitem/MenuItem";
 
-import './SpecialMenu.css';
-
-const SpecialMenu = () => (
-  <div>
-    SpecialMenu
-  </div>
-);
+import "./SpecialMenu.css";
+import images from "../../constants/images";
+import data from "../../constants/data";
+const SpecialMenu = () => {
+  const [menudata, setMenudata] = useState(data);
+  return (
+    <div className="section__padding bg__black">
+      <div>
+        <SubHeading title="Menu That Fits You Palatte" />
+        <h1 className="p__opensans">Today's Special</h1>
+      </div>
+      <div className="app__specialMenu-content">
+        <div>
+          {menudata.wines.map((menu) => {
+            return (
+              <MenuItem
+                title={menu.title}
+                tags={menu.tags}
+                price={menu.price}
+              />
+            );
+          })}
+        </div>
+        
+        <div>
+          {menudata.cocktails.map((menu) => {
+            return (
+              <MenuItem
+                title={menu.title}
+                tags={menu.tags}
+                price={menu.price}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SpecialMenu;
